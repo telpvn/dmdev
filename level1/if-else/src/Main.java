ca6aa198140976717d0ba17a54f18620ed04efb5
telpvn <telpvn@mail.ru>
2024-04-30 17:43:32 +0400

    Commit 2

--- level1/if-else/src/Main.java
+++ level1/if-else/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        int value = 10;
+//        boolean test = value > 0;
+        if (value > 0) {
+            System.out.println("Число является положительным");
+        } else if (value < 0) {
+            System.out.println("Число является отрицательным");
+        } else {
+            System.out.println("Число является нулем");
+        }
     }
 }
