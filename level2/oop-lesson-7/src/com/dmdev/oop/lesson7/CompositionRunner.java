827f7d29f0a13651f43e9234268f6ea0baba9c3b
telpvn <telpvn@mail.ru>
2024-05-13 14:19:05 +0400

    Class composition 2

--- level2/oop-lesson-7/src/com/dmdev/oop/lesson7/CompositionRunner.java
+++ level2/oop-lesson-7/src/com/dmdev/oop/lesson7/CompositionRunner.java
@@ -3,7 +3,8 @@ package com.dmdev.oop.lesson7;
 public class CompositionRunner {
     public static void main(String[] args) {
         Ram ram = new Ram(1000);
-        Computer computer = new Computer(ram);
+        Ssd ssd = new Ssd(500);
+        Computer computer = new Computer(ram, ssd);
         computer.printState();
     }
 }
--- level2/oop-lesson-7/src/com/dmdev/oop/lesson7/Computer.java
+++ level2/oop-lesson-7/src/com/dmdev/oop/lesson7/Computer.java
@@ -3,12 +3,14 @@ package com.dmdev.oop.lesson7;
 public class Computer {
 
     private Ram ram;
+    private Ssd ssd;
 
-    public Computer(Ram ram) {
+    public Computer(Ram ram, Ssd ssd) {
         this.ram = ram;
+        this.ssd = ssd;
     }
 
     public void printState() {
-        System.out.println("Computer: ram " + ram.getValue());
+        System.out.println("Computer: ram " + ram.getValue() + ", ssd " + ssd.getValue());
     }
 }
