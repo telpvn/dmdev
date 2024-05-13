998c30fbdbaf6c0f16b33da24896b1db5dd42561
telpvn <telpvn@mail.ru>
2024-05-13 11:33:53 +0400

    Constructors 2

--- level2/oop-lesson-1/src/com/dmdev/oop/lesson2/Computer.java
+++ level2/oop-lesson-1/src/com/dmdev/oop/lesson2/Computer.java
@@ -8,12 +8,24 @@ public class Computer {
         System.out.println("Я был создан");
     }
 
+    Computer(int newSsd) {
+        ssd = newSsd;
+        ram = 999;
+    }
+
+    Computer(int newSsd, int newRam) {
+        ssd = newSsd;
+        ram = newRam;
+    }
+
+
     void load() {
         System.out.println("Я загрузился");
     }
 
     void printState() {
-        System.out.println("Ssd" + ssd);
-        System.out.println("Ram" + ram);
+        System.out.println("Ssd " + ssd);
+        System.out.println("Ram " + ram);
+        System.out.println();
     }
 }
--- level2/oop-lesson-1/src/com/dmdev/oop/lesson2/ComputerRunner.java
+++ level2/oop-lesson-1/src/com/dmdev/oop/lesson2/ComputerRunner.java
@@ -4,5 +4,12 @@ public class ComputerRunner {
     public static void main(String[] args) {
         int value = 5;
         Computer computer = new Computer();
+        computer.printState();
+
+        Computer computer2 = new Computer(1050);
+        computer2.printState();
+
+        Computer computer3 = new Computer(10000, 10000);
+        computer3.printState();
     }
 }
