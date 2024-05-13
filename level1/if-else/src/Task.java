12981ae39df0956afd6afa3abfc28ae7e3cbb859
telpvn <telpvn@mail.ru>
2024-05-13 08:32:08 +0400

    OOP. Introduction

---
+++ level1/if-else/src/Task.java
@@ -0,0 +1,9 @@
+/**
+ * 1. Дан порядковый номер месяца. Вывести на консоль пору года, к которой относится месяц.
+ */
+public class Task {
+    public static void main(String[] args) {
+        int month = 1;
+//        зима 
+    }
+}
