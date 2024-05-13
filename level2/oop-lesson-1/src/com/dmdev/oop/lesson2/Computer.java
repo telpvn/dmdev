3d51f4787fecc3379a2f93da55e2c608eb99f8f6
telpvn <telpvn@mail.ru>
2024-05-13 09:32:51 +0400

    Classes and objects 3

--- level2/oop-lesson-1/src/com/dmdev/oop/lesson2/Computer.java
+++ level2/oop-lesson-1/src/com/dmdev/oop/lesson2/Computer.java
@@ -2,9 +2,14 @@ package com.dmdev.oop.lesson2;
 
 public class Computer {
     int ssd = 500;
-    int ram = 1024;
+    int ram;
 
     void load() {
         System.out.println("Я загрузился");
+    }
+
+    void printState() {
+        System.out.println("Ssd" + ssd);
+        System.out.println("Ram" + ram);
     }
 }
