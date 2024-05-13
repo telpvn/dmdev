c33003c6942805e504153bf8f54148b706355578
telpvn <telpvn@mail.ru>
2024-05-13 08:56:12 +0400

    Classes and objects 1

---
+++ level2/oop-lesson-1/src/com/dmdev/oop/lesson2/ComputerRunner.java
@@ -0,0 +1,8 @@
+package com.dmdev.oop.lesson2;
+
+public class ComputerRunner {
+    public static void main(String[] args) {
+        Computer computer = new Computer();
+        computer.load();
+    }
+}
