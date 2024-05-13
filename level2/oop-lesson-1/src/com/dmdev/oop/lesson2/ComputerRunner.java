9733eac8cd1133790e4270c262446dfa6105aaff
telpvn <telpvn@mail.ru>
2024-05-13 09:02:35 +0400

    Classes and objects 2

--- level2/oop-lesson-1/src/com/dmdev/oop/lesson2/ComputerRunner.java
+++ level2/oop-lesson-1/src/com/dmdev/oop/lesson2/ComputerRunner.java
@@ -4,5 +4,7 @@ public class ComputerRunner {
     public static void main(String[] args) {
         Computer computer = new Computer();
         computer.load();
+        System.out.println("Ram: " + computer.ram);
+        System.out.println("Ssd: " + computer.ssd);
     }
 }
