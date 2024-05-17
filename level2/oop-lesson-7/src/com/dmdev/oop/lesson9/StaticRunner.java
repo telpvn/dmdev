01f4b01f1999a8d375d71f09ea6d11467c10680d
telpvn <telpvn@mail.ru>
2024-05-17 06:57:58 +0400

    Modifier static 2

--- level2/oop-lesson-7/src/com/dmdev/oop/lesson9/StaticRunner.java
+++ level2/oop-lesson-7/src/com/dmdev/oop/lesson9/StaticRunner.java
@@ -6,13 +6,16 @@ public class StaticRunner {
     public static void main(String[] args) {
         new Computer(null, null);
         new Computer(null, null);
-        new Computer(null, null);
+        Computer computer1 = new Computer(null, null);
         Computer computer2 = new Computer(null, null);
         Computer computer = new Computer(null, null);
         Class<? extends Computer> clazz = computer.getClass();
         Class<? extends Computer> clazz2 = computer2.getClass();
+        Class<? extends Computer> clazz1 = computer1.getClass();
+
         System.out.println(clazz == clazz2);
         System.out.println(computer == computer2);
+
 
     }
 }
