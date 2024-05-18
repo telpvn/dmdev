29e8fdd012ad4ad4e521e9bf7cb0881b710075c0
telpvn <telpvn@mail.ru>
2024-05-18 11:56:41 +0400

    StringBuffer

--- level2/lesson-10/src/com/dmdev/lesson10/Task6.java
+++ level2/lesson-10/src/com/dmdev/lesson10/Task6.java
@@ -3,9 +3,9 @@ package com.dmdev.lesson10;
 public class Task6 {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        String value = "";
+        StringBuffer value = new StringBuffer();
         for (int i = 0; i < 100_000; i++) {
-            value += i;
+            value.append(i);
         }
         long endTime = System.currentTimeMillis();
         System.out.println("Time: " + (endTime - startTime));
