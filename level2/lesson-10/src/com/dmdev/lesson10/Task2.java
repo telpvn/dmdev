0af9ea2bf67a68b6078ec5ac5b945b3ccf745b68
telpvn <telpvn@mail.ru>
2024-05-18 11:39:32 +0400

    Task String

---
+++ level2/lesson-10/src/com/dmdev/lesson10/Task2.java
@@ -0,0 +1,17 @@
+package com.dmdev.lesson10;
+
+/**
+ * 2. Написать функцию, принимающую 2 параметра: строку и слово - и
+ * возвращающую true, если строка начинается и заканчивается этим словом.
+ */
+
+public class Task2 {
+    public static void main(String[] args) {
+        String value = "123 test test 1234";
+        String word = "123";
+        System.out.println(isStartAndEnd(value, word));
+    }
+    private static boolean isStartAndEnd(String target, String word) {
+       return target.startsWith(word) && target.endsWith(word);
+    }
+}
