79502d301fa96b2b583388cda30864965d517ecd
telpvn <telpvn@mail.ru>
2024-05-27 12:16:05 +0400

    Innovation interfaces

--- level2/oop-lesson11/src/com/dmdev/lesson11/Printable.java
+++ level2/oop-lesson11/src/com/dmdev/lesson11/Printable.java
@@ -1,8 +1,21 @@
 package com.dmdev.lesson11;
 
+import java.util.Random;
+
 public interface Printable {
 
     String SOME_VALUE = "Example";
+    Random RANDOM = new Random();
+
+    default void printWithRandom() {
+        Random random = new Random();
+        System.out.println(generateRandom());
+        print();
+    }
 
     void print();
+
+    private int generateRandom() {
+        return RANDOM.nextInt();
+    }
 }
