cb402eda2ebaf75f51cabf3e96c0663e2035113e
telpvn <telpvn@mail.ru>
2024-05-30 18:07:10 +0400

    Enum 2

--- level2/oop-lesson-15/src/com/dmdev/lesson16/EnumRunner.java
+++ level2/oop-lesson-15/src/com/dmdev/lesson16/EnumRunner.java
@@ -1,9 +1,16 @@
 package com.dmdev.lesson16;
 
+import java.util.Arrays;
+
 public class EnumRunner {
 
     public static void main(String[] args) {
         ProcessorType processorType = ProcessorType.BIT_32;
         System.out.println(processorType);
+        System.out.println(processorType.name());
+        System.out.println(processorType.toString());
+        System.out.println(processorType.valueOf("BIT_32"));
+        System.out.println(Arrays.toString(processorType.values()));
+        System.out.println(processorType.BIT_32.ordinal());
     }
 }
