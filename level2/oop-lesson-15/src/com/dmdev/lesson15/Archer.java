7ad6e8b16c0f85c41a69488bdf8c390aaee7cb06
telpvn <telpvn@mail.ru>
2024-05-30 17:13:12 +0400

    Inner and nested classes 1

--- level2/oop-lesson-15/src/com/dmdev/lesson15/Archer.java
+++ level2/oop-lesson-15/src/com/dmdev/lesson15/Archer.java
@@ -2,13 +2,33 @@ package com.dmdev.lesson15;
 
 public class Archer extends Hero {
 
+    private Wolf wolf;
+
     public Archer(String name, int damage) {
         super(name, damage);
+        this.wolf = new Wolf("Wolf", 7);
     }
 
     @Override
     public void attackEnemy(Enemy enemy) {
         System.out.println(getName() + " стрельнул из лука в " + enemy.getName());
-        enemy.takeDamage(getDamage());
+        wolf.attackEnemy(enemy);
+    }
+
+    private class Wolf {
+
+        private String name;
+        private int damage;
+
+        public Wolf(String name, int damage) {
+            this.name = name;
+            this.damage = damage;
+        }
+
+        public void attackEnemy(Enemy enemy) {
+            System.out.println(name + " и " + Archer.this.getName() + " наносят совмесный урон");
+            enemy.takeDamage(damage + Archer.this.getDamage());
+        }
     }
 }
+
