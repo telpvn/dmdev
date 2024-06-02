e07ae9e5eaf7b4a0c19c15694cc44b32f70bd1d7
telpvn <telpvn@mail.ru>
2024-06-02 17:52:39 +0400

    Subtyping 1

---
+++ level2/oop-lesson-15/src/zaur_tregulov/generics/Test2.java
@@ -0,0 +1,23 @@
+package zaur_tregulov.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test2 {
+    public static void main(String[] args) {
+//        X x = new Y();
+//        List<X> list1 = new ArrayList<X>();
+
+//        List<Number> list = new ArrayList<Integer>();
+//        list.add(18);
+//        list.add(3.14);
+    }
+}
+
+class X {
+
+}
+
+class Y extends X {
+
+}
