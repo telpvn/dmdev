9102e169138ed35006a945c97df5f96b7120fced
telpvn <telpvn@mail.ru>
2024-06-02 10:27:28 +0400

    Parameterized class 3

---
+++ level2/oop-lesson-15/src/zaur_tregulov/generics/ParameterizedClass2.java
@@ -0,0 +1,29 @@
+package zaur_tregulov.generics;
+
+public class ParameterizedClass2 {
+    public static void main(String[] args) {
+        Pair<String, Integer> pair1 = new Pair<>("privet", 20);
+        System.out.println("Znacheniya pari: value1 = " + pair1.getFirstValue() + ", value2 = " + pair1.getSecondValue());
+
+        Pair<Integer, Double> pair2 = new Pair<>(156, 3.14);
+        System.out.println("Znacheniya pari: value1 = " + pair2.getFirstValue() + ", value2 = " + pair1.getSecondValue());
+    }
+}
+
+class Pair<V1, V2> {
+    private V1 value1;
+    private V2 value2;
+
+    public Pair(V1 value1, V2 value2) {
+        this.value1 = value1;
+        this.value2 = value2;
+    }
+
+    public V1 getFirstValue() {
+        return value1;
+    }
+
+    public V2 getSecondValue() {
+        return value2;
+    }
+}
