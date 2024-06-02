2633ef8d4b9e317ba00e52408a6a0edd5f8eda0a
telpvn <telpvn@mail.ru>
2024-06-02 08:31:02 +0400

    Introduction to generics 3

--- level2/oop-lesson-15/src/saur_tregulov/generics/Test1.java
+++ level2/oop-lesson-15/src/saur_tregulov/generics/Test1.java
@@ -14,6 +14,10 @@ public class Test1 {
         list.add("poka");
         list.add("ok");
         list.add("rest");
+        list.add(new Car());
+
+//        Exception in thread "main" java.lang.ClassCastException: class saur_tregulov.generics.Car cannot be cast to class java.lang.String (saur_tregulov.generics.Car is in unnamed module of loader 'app'; java.lang.String is in module java.base of loader 'bootstrap')
+//        at saur_tregulov.generics.Test1.main(Test1.java:20)
 
         for (Object o : list) {
             System.out.println(o + " dlina " + ((String)o).length());
