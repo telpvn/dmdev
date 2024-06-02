c2f3fa486c04dc72cdd400f3bbdb55cfc9a035b8
telpvn <telpvn@mail.ru>
2024-06-02 08:26:02 +0400

    Introduction to generics 2

--- level2/oop-lesson-15/src/saur_tregulov/generics/Test1.java
+++ level2/oop-lesson-15/src/saur_tregulov/generics/Test1.java
@@ -6,10 +6,18 @@ import java.util.List;
 public class Test1 {
     public static void main(String[] args) {
         List list = new ArrayList();
-        list.add("OK");
-        list.add(5);
-        list.add(new StringBuffer("test"));
-        list.add(new Car());
+//        list.add("OK");
+//        list.add(5);
+//        list.add(new StringBuffer("test"));
+//        list.add(new Car());
+        list.add("privet");
+        list.add("poka");
+        list.add("ok");
+        list.add("rest");
+
+        for (Object o : list) {
+            System.out.println(o + " dlina " + ((String)o).length());
+        }
 
     }
 }
