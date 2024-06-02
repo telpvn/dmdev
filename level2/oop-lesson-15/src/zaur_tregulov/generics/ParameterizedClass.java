907a4f383df6f8617c25ff9ecbfb61dfb97988fc
telpvn <telpvn@mail.ru>
2024-06-02 09:22:39 +0400

    Parameterized class 1

---
+++ level2/oop-lesson-15/src/zaur_tregulov/generics/ParameterizedClass.java
@@ -0,0 +1,20 @@
+package zaur_tregulov.generics;
+
+public class ParameterizedClass {
+    public static void main(String[] args) {
+        Info<String> info1 = new Info<>("privet");
+        System.out.println(info1);
+    }
+}
+
+class Info<T> {
+    private T value;
+
+    public Info(T value) {
+        this.value = value;
+    }
+
+    public String toString() {
+        return "{[" + value + "]}";
+    }
+}
--- level2/oop-lesson-15/src/zaur_tregulov/generics/Test1.java
+++ level2/oop-lesson-15/src/zaur_tregulov/generics/Test1.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Test1 {
     public static void main(String[] args) {
-        List list = new ArrayList();
+        List<String> list = new ArrayList<>();
 //        list.add("OK");
 //        list.add(5);
 //        list.add(new StringBuffer("test"));
@@ -14,7 +14,7 @@ public class Test1 {
         list.add("poka");
         list.add("ok");
         list.add("rest");
-        list.add(new Car());
+//        list.add(new Car());
 
 //        Exception in thread "main" java.lang.ClassCastException: class zaur_tregulov.generics.Car cannot be cast to class java.lang.String (zaur_tregulov.generics.Car is in unnamed module of loader 'app'; java.lang.String is in module java.base of loader 'bootstrap')
 //        at zaur_tregulov.generics.Test1.main(Test1.java:20)
