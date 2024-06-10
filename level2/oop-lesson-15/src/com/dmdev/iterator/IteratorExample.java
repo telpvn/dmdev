50b53e52f54d256197d8fd47ca93f67140c651c8
telpvn <telpvn@mail.ru>
2024-06-10 08:41:30 +0400

    Iterator. Theory 2

--- level2/oop-lesson-15/src/com/dmdev/iterator/IteratorExample.java
+++ level2/oop-lesson-15/src/com/dmdev/iterator/IteratorExample.java
@@ -1,11 +1,13 @@
 package com.dmdev.iterator;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class IteratorExample {
     public static void main(String[] args) {
-        List<Integer> list = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> sourcelist = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> list = new ArrayList<>(sourcelist);
 
 //        for (int i = 0; i < list.size(); i++) {
 //            Integer integer = list.get(i);
@@ -17,8 +19,12 @@ public class IteratorExample {
 
         for (Iterator<Integer> iterator = list.iterator(); iterator.hasNext();) {
             Integer next = iterator.next();
-            System.out.println(next);
+            if (next == 3 || next == 4) {
+                iterator.remove();
+            }
         }
+
+        System.out.println(list);
 
 //        Iterator<Integer> iterator = list.iterator();
 //        while (iterator.hasNext()) {
