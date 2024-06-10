13ec21ed415b2b38909e2d00cffc64fa0684e306
telpvn <telpvn@mail.ru>
2024-06-10 13:32:19 +0400

    Методы equals и hashCode 4

--- level2/lesson19/src/com/dmdev/lesson19/person/PersonExample.java
+++ level2/lesson19/src/com/dmdev/lesson19/person/PersonExample.java
@@ -4,7 +4,7 @@ public class PersonExample {
 
     public static void main(String[] args) {
         Person ivan = new Person(1, "Ivan", "Ivanov");
-        Person petr = new Person(1, "Ivan", "Ivanov");
+        Person petr = new Person(2, "Ivan", "Ivanov");
 //        Person petr = new Person(2, "Petr", "Petrov");
 
         System.out.println(ivan.hashCode());
