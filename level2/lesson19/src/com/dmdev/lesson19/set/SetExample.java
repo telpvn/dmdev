0b1265428eca03189a12f89132e0752280fb443b
telpvn <telpvn@mail.ru>
2024-06-12 15:00:28 +0400

    Set 2

--- level2/lesson19/src/com/dmdev/lesson19/set/SetExample.java
+++ level2/lesson19/src/com/dmdev/lesson19/set/SetExample.java
@@ -7,14 +7,12 @@ public class SetExample {
 
     public static void main(String[] args) {
         Set<String> set = new HashSet<>();
-        set.add("aaa");
-        set.add("bbb");
-        set.add("ccc");
-        set.add("ddd");
-        set.add("bbb");
+         System.out.println(set.add("aaa"));
+         System.out.println(set.add("bbb"));
+         System.out.println(set.add("ccc"));
+         System.out.println(set.add("ddd"));
+         System.out.println(set.add("bbb"));
 
         System.out.println(set);
     }
-
-
 }
