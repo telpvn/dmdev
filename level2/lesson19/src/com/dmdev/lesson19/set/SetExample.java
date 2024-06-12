bfda8e90d26df0c93c938dd4ca73b9b657829dc1
telpvn <telpvn@mail.ru>
2024-06-12 15:12:50 +0400

    Set 3

--- level2/lesson19/src/com/dmdev/lesson19/set/SetExample.java
+++ level2/lesson19/src/com/dmdev/lesson19/set/SetExample.java
@@ -1,12 +1,16 @@
 package com.dmdev.lesson19.set;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SetExample {
 
     public static void main(String[] args) {
-        Set<String> set = new HashSet<>();
+        new HashSet<>();
+        new LinkedHashSet<>();
+        Set<String> set = new TreeSet<>();
          System.out.println(set.add("aaa"));
          System.out.println(set.add("bbb"));
          System.out.println(set.add("ccc"));
@@ -14,5 +18,9 @@ public class SetExample {
          System.out.println(set.add("bbb"));
 
         System.out.println(set);
+
+        for (String value : set) {
+            System.out.println(value);
+        }
     }
 }
