1962351a93509b3bbeff507f7e3a86733320ffa7
telpvn <telpvn@mail.ru>
2024-06-12 09:10:56 +0400

    Sorting collections 2

--- level2/lesson19/src/com/dmdev/lesson19/map/SortExample.java
+++ level2/lesson19/src/com/dmdev/lesson19/map/SortExample.java
@@ -4,15 +4,16 @@ import com.dmdev.lesson19.person.Person;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class SortExample {
 
     public static void main(String[] args) {
 
-        List<String> strings = Arrays.asList("123", "345", "14", "678");
-        Collections.sort(strings);
-        System.out.println(strings);
+//        List<String> strings = Arrays.asList("123", "345", "14", "678");
+//        Collections.sort(strings);
+//        System.out.println(strings);
 
         List<Person> personList = Arrays.asList(
                 new Person(1, "Ivan", "Ivanov"),
@@ -21,5 +22,16 @@ public class SortExample {
         );
         Collections.sort(personList);
         System.out.println(personList);
+
+        personList.sort(new FirstNameComparator());
+        System.out.println(personList);
+    }
+
+    public static class FirstNameComparator implements Comparator<Person> {
+
+        @Override
+        public int compare(Person o1, Person o2) {
+            return o1.getFirstName().compareTo(o2.getFirstName());
+        }
     }
 }
