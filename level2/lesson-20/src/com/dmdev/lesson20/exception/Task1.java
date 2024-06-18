39c3114587f094fa8b1e68760c0b3c6fa314c175
telpvn <telpvn@mail.ru>
2024-06-18 16:22:32 +0400

    Task1 1

---
+++ level2/lesson-20/src/com/dmdev/lesson20/exception/Task1.java
@@ -0,0 +1,13 @@
+package com.dmdev.lesson20.exception;
+
+/**
+ * 1. Объявите переменную со значением null. Вызовите метод у этой
+ * переменной. Отловите возникшее исключение.
+ */
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        String value = null;
+        value.length();    }
+}
