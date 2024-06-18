3b433527278c5390e0ff348e84261143fab67563
telpvn <telpvn@mail.ru>
2024-06-18 16:28:23 +0400

    Task1 2

--- level2/lesson-20/src/com/dmdev/lesson20/exception/Task1.java
+++ level2/lesson-20/src/com/dmdev/lesson20/exception/Task1.java
@@ -9,6 +9,11 @@ public class Task1 {
 
     public static void main(String[] args) {
         String value = null;
+        try {
         value.length();
+        } catch (NullPointerException exception) {
+            System.err.println("Catch");
+            exception.printStackTrace();
+        }
     }
 }
