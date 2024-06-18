bac650baa42d4358c529b67968891669e085c655
telpvn <telpvn@mail.ru>
2024-06-18 15:40:41 +0400

    Exception Handling. Theory. Part 2 3

--- level2/lesson-20/src/com/dmdev/lesson20/exception/ExceptionExample.java
+++ level2/lesson-20/src/com/dmdev/lesson20/exception/ExceptionExample.java
@@ -8,12 +8,23 @@ public class ExceptionExample {
     public static void main(String[] args) throws FileNotFoundException, TimeoutException {
         System.out.println("main start");
         try {
+            System.out.println(finallyTest());
             unsafe(-10);
         } finally {
             System.out.println("finally");
         }
         System.out.println("main code");
 //        code
+    }
+
+    public static int finallyTest() {
+        try {
+            return 2;
+        } catch (Throwable throwable) {
+            return 3;
+        } finally {
+            return 4;
+        }
     }
 
     public static void unsafe(int value) throws FileNotFoundException, TimeoutException {
