4cce40f14993be655f3576ffda47f88340e6634e
telpvn <telpvn@mail.ru>
2024-06-20 13:15:23 +0400

    Task5 1

---
+++ level2/lesson-20/src/com/dmdev/lesson20/exception/Task5.java
@@ -0,0 +1,31 @@
+package com.dmdev.lesson20.exception;
+
+/**
+ * 5. Бросить одно из существующих в JDK исключений, отловить его и
+ * выбросить своё собственное, указав в качестве причины отловленное.
+ */
+
+public class Task5 {
+
+    public static void main(String[] args) {
+        try {
+            catchCustomException();
+        } catch (DmdevException exception) {
+            System.out.println("catched in main");
+        }
+    }
+
+    public static void catchCustomException() {
+        try {
+            unsafe();
+        } catch (RuntimeException exception) {
+            System.out.println("Catch int catchCustomException method");
+            throw new DmdevException(exception);
+        }
+    }
+
+    public static void unsafe() {
+        throw new RuntimeException("Ooops");
+    }
+}
+
