6a2ebcee9493ded35eb476d4dedd1e8d69a5be78
telpvn <telpvn@mail.ru>
2024-06-20 19:28:12 +0400

    Lambda expressions 4

--- level2/lesson-20/src/com/dmdev/lesson21/LambdaExample.java
+++ level2/lesson-20/src/com/dmdev/lesson21/LambdaExample.java
@@ -4,6 +4,12 @@ import java.util.Comparator;
 
 public class LambdaExample {
     public static void main(String[] args) {
+        Comparator<Integer> comparator = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return 0;
+            }
+        }
         Comparator<Integer> comparator = (o1, o2) -> Integer.compare(o1, o2);
 
         System.out.println(comparator.compare(25, 100));
