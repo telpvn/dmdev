2318b278ea1bd85fa60f092ef2acb53dca2f149a
telpvn <telpvn@mail.ru>
2024-06-20 18:44:17 +0400

    Lambda expressions 1

---
+++ level2/lesson-20/src/com/dmdev/lesson21/LambdaExample.java
@@ -0,0 +1,18 @@
+package com.dmdev.lesson21;
+
+import java.util.Comparator;
+
+public class LambdaExample {
+    public static void main(String[] args) {
+        Comparator<Integer> comparator = new IntegerComparator();
+        System.out.println(comparator.compare(25, 100));
+    }
+
+    private static class IntegerComparator implements Comparator<Integer> {
+
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return Integer.compare(o1, o2);
+        }
+    }
+}
