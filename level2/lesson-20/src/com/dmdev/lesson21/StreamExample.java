1d70b3da8f252e2c5ca73068d8c52d6041aebfb4
telpvn <telpvn@mail.ru>
2024-06-21 10:07:20 +0400

    Stream 2

--- level2/lesson-20/src/com/dmdev/lesson21/StreamExample.java
+++ level2/lesson-20/src/com/dmdev/lesson21/StreamExample.java
@@ -6,12 +6,18 @@ public class StreamExample {
 
     public static void main(String[] args) {
         List<String> strings = List.of("11", "22", "33", "44", "55");
-        for (String string : strings) {
-            String value = string + string;
-            Integer intValue = Integer.valueOf(value);
-            if (intValue % 2 == 0) {
-                System.out.println(intValue);
-            }
-        }
+        strings.stream()
+                .map(string -> string + string)
+                .map(Integer::valueOf)
+                .filter(value -> value % 2 == 0)
+                .forEach(System.out::println);
+
+//        for (String string : strings) {
+//            String value = string + string;
+//            Integer intValue = Integer.valueOf(value);
+//            if (intValue % 2 == 0) {
+//                System.out.println(intValue);
+//            }
+//        }
     }
 }
