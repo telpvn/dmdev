f2e859eb5c76d60467625dcd0437f0fd9634f560
telpvn <telpvn@mail.ru>
2024-06-27 16:15:10 +0400

    MapReduce 1

---
+++ level2/lesson-20/src/com/dmdev/lesson21/MapReduceExample.java
@@ -0,0 +1,29 @@
+package com.dmdev.lesson21;
+
+import java.util.stream.Stream;
+
+public class MapReduceExample {
+
+    /**
+     * [1000_000] -> age -> max
+     * <p>
+     * [1000_000] -> age -> max -> max
+     * <p>
+     * [1000_000] -> age -> max
+     */
+
+    public static void main(String[] args) {
+        Stream.of(
+                        new Student(18, "Ivan"),
+                        new Student(23, "Petr"),
+                        new Student(34, "Vasya"),
+                        new Student(45, "Sveta"),
+                        new Student(20, "Katya"),
+                        new Student(68, "Den"),
+                        new Student(101, "Kira")
+                )
+                .map(Student::getAge)
+                .reduce(Math::max)
+                .ifPresent(System.out::println);
+    }
+}
---
+++ level2/lesson-20/src/com/dmdev/lesson21/Student.java
@@ -0,0 +1,22 @@
+package com.dmdev.lesson21;
+
+import java.util.stream.Stream;
+
+public class Student {
+
+    private int age;
+    private String name;
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
