571f1f1ad79c5e791c9a8939d89d89232c3c4e36
telpvn <telpvn@mail.ru>
2024-06-28 17:41:22 +0400

    Task4 2

--- level2/lesson-20/src/com/dmdev/lesson21/task/Task4.java
+++ level2/lesson-20/src/com/dmdev/lesson21/task/Task4.java
@@ -17,7 +17,7 @@ public class Task4 {
         List<Integer> integers = List.of(5, 2, 4, 2, 1);
         String result = integers.stream()
                 .map(String::valueOf)
-                .collect(Collectors.joining());
+                .collect(Collectors.joining(",", "Prefix: ", " end" ));
         System.out.println(result);
     }
 }
