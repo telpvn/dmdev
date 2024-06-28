e0c9744f6b2687e22e214832b68c53aad084c69f
telpvn <telpvn@mail.ru>
2024-06-28 17:24:17 +0400

    Task3 1

---
+++ level2/lesson-20/src/com/dmdev/lesson21/task/Task3.java
@@ -0,0 +1,28 @@
+package com.dmdev.lesson21.task;
+
+import java.util.Map;
+
+/**
+ * 3. Дана Map<String, Integer>. Найти сумму всех
+ * значений, длина ключей которых меньше 7
+ * символов.
+ */
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        Map<String, Integer> map = Map.of(
+                "string1", 1,
+                "strin2", 2,
+                "string3", 3,
+                "string4", 5,
+                "strin5", 5
+        );
+        int result = map.entrySet().stream()
+                .filter(entry -> entry.getKey().length() < 7)
+                .mapToInt(Map.Entry::getValue)
+                .sum();
+        System.out.println(result);
+
+    }
+}
