19a7db5c0ad7127897dd1b083d2c015e2e98853c
telpvn <telpvn@mail.ru>
2024-06-28 18:06:19 +0400

    Task5 1

---
+++ level2/lesson-20/src/com/dmdev/lesson21/task/Person.java
@@ -0,0 +1,30 @@
+package com.dmdev.lesson21.task;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ level2/lesson-20/src/com/dmdev/lesson21/task/Task5.java
@@ -0,0 +1,33 @@
+package com.dmdev.lesson21.task;
+
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 5. Дан класс Person с полями firstName, lastName,
+ * age.
+ * Вывести полное имя самого старшего человека, у
+ * которого длина этого имени не превышает 15
+ * символов.
+ */
+
+public class Task5 {
+
+    public static void main(String[] args) {
+        List<Person> persons = List.of(
+                new Person("Ivan", "Ivanov", 20),
+                new Person("Petr", "Petrov", 25),
+                new Person("Sveta", "Svetikova", 33),
+                new Person("Kate", "Ivanova", 45),
+                new Person("Slava", "Slavikov", 18),
+                new Person("Arni", "Kutuzov", 56)
+        );
+
+        persons.stream()
+                .filter(person -> person.getFullName().length() < 15)
+                .max(Comparator.comparing(Person::getAge))
+                .map(Person::getFullName)
+                .ifPresent(System.out::println);
+
+    }
+}
