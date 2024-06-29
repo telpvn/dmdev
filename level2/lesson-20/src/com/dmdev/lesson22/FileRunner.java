14bd71127bcb7e021afa4a753652d3e05937d56f
telpvn <telpvn@mail.ru>
2024-06-29 12:57:27 +0400

    File class 2

--- level2/lesson-20/src/com/dmdev/lesson22/FileRunner.java
+++ level2/lesson-20/src/com/dmdev/lesson22/FileRunner.java
@@ -1,11 +1,15 @@
 package com.dmdev.lesson22;
 
 import java.io.File;
+import java.io.IOException;
 
 public class FileRunner {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File file = new File("resources/test.txt");
+        System.out.println(file.createNewFile());
         System.out.println(file.exists());
+        System.out.println(file.isFile());
+        System.out.println(file.isDirectory());
     }
 }
