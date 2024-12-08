eeb12cb45580bd793b6f7ef16b917fc56b627192
telpvn <telpvn@mail.ru>
2024-12-08 13:58:44 +0400

    Switch statement

---
+++ lesson-4/src/com/dmdev/lesson4/SwitchExample.java
@@ -0,0 +1,26 @@
+package com.dmdev.lesson4;
+
+public class SwitchExample {
+
+    public static void main(String[] args) {
+        int value = 2;
+        switch (value) {
+            case 1:
+                System.out.println("value == 1");
+                break;
+            case 2:
+            case 3:
+            case 4:
+                System.out.println("value == 2, 3, 4");
+                break;
+            case 10:
+                System.out.println("value == 10");
+                break;
+            case 28:
+                System.out.println("value == 28");
+                break;
+            default:
+                System.out.println("Нет такого значения");
+        }
+    }
+}
